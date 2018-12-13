cd6dedcfc1957e936f32c8a9f748d6191b6672f8
brokenjoints <45832753+brokenjoints@users.noreply.github.com>
2018-12-13 10:18:43 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,11 +3,12 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log('I am ready!');
+  client.user.setActivity("relow help");
 });
 
 client.on('message', message => {
   if (message.content === 'relow help') {
-    message.channel.send('__RELOWBOT FUNCTIONS__ :RELOW_square:\n\n**relow key**\n**relow playtest**\n**relow tournament**\n**relow info**');
+    message.channel.send('__RELOWBOT FUNCTIONS__\n\n**relow key**\n**relow playtest**\n**relow tournament**\n**relow info**');
   }
   if (message.content === 'relow key') {
     message.channel.send('I will never give you a key');
@@ -16,7 +17,7 @@ client.on('message', message => {
     message.channel.send(':video_game: there is no playtest at the moment\n:bell: playtests will be announced in '+ message.guild.channels.find(channel => channel.name === "playtest-schedule").toString());
   }
   if (message.content === 'relow tournament') {
-    message.channel.send(':fire: there is a tournament in progress\nread more in' + message.guild.channels.find(channel => channel.name === "tournament-rules").toString());
+    message.channel.send(':fire: there is a tournament in progress\n:information_source: read more in' + message.guild.channels.find(channel => channel.name === "tournament-rules").toString());
   }
 
 });
