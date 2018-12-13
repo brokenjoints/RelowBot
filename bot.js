0e7e42047a368981ba3909334fbef17918a449f3
brokenjoints <45832753+brokenjoints@users.noreply.github.com>
2018-12-13 09:44:46 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,13 +7,16 @@ client.on('ready', () => {
 
 client.on('message', message => {
   if (message.content === 'relow help') {
-    message.channel.send('__RELOWBOT__\n\n**relow key**\n**relow playtest**\n**relow tournament**\n**relow info**');
+    message.channel.send('__RELOWBOT FUNCTIONS__ :RELOW_square:n\n**relow key**\n**relow playtest**\n**relow tournament**\n**relow info**');
   }
   if (message.content === 'relow key') {
     message.channel.send('I will never give you a key');
   }
   if (message.content === 'relow playtest') {
-    message.channel.send(':video_game: there is no playtest at the moment\n:bell: turn on notification of #playtest-schedule');
+    message.channel.send(':video_game: there is no playtest at the moment\n:bell: playtests will be announced in \#playtest-schedule');
+  }
+  if (message.content === 'relow tournament') {
+    message.channel.send(':fire: there is a tournament in progress\n');
   }
 
 });
