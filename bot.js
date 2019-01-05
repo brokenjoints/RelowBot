2257342e545310437af82e3effee97e92c4651c0
brokenjoints <45832753+brokenjoints@users.noreply.github.com>
2019-01-05 14:41:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,11 @@ client.on('guildMemberAdd', member => {
 
 + "__QUESTIONS?__\n"
 + "Use the <#484619958395863050> channel.\n\n"
-
+                     
++ "Follow the development also on Twitter and YouTube\n"
++ "https://twitter.com/brokenjoints_\n"
++ "https://www.youtube.com/channel/UCtJGCNnXi0KGdRtvN_QBITQ\nn"
+                     
 + "**Have a nice day and invite to support the game!**\n"
 + "https://discord.gg/bCUvtnz"); 
 });
@@ -21,50 +25,21 @@ client.on('message', message => {
   if (message.author.bot) return undefined;
   
   if (message.content === 'relow help' || message.content === 'Relow help') {
-    message.channel.send("__RELOWBOT FUNCTIONS__\n\n"
-                         + "__INFO__\n"
-                         + "**relow key**\n"
-                         + "**relow playtest**\n"
-                         + "**relow signup**\n"
-                         + "**relow date**\n"
-                         + "**relow platform**\n"
-                         + "**relow price**\n\n"
-                         + "__RELOW WIKI__\n"
-                         + "**relow weapons**\n"
-                         + "**relow armors**\n"
-                         + "**relow biomes**\n\n"
-                         + "__RELOW ESPORT__\n"
-                         + "**relow esport**\n\n"
-                         + "__RELOW LINKS__\n"
-                         + "**relow links**");
-  }
-  if (message.content === 'relow key' || message.content === 'Relow key') {
-    message.channel.send("**RELOW KEY**\n\n"
-                         + "__HOW TO RECEIVE A KEY__\n\n"
-                         + "**the alpha sign up isn’t started yet**\n\n"
-                         + "__HOW TO REDEEM A KEY__\n"
-                         + "click the button in the bottom left corner **+ ADD GAME...**\n"
-                         + "**activate a product on Steam**\n"
-                         + "follow the prompts and paste the key code");
-  }
-  if (message.content === 'relow playtest' || message.content === 'Relow playtest') {
-    message.channel.send("**WIP**\n\n");
-  }
-  if (message.content === 'relow tournament' || message.content === 'Relow tournament') {
-    message.channel.send("**WIP**\n\n");
-  }
-  if(message.content === 'relow info' || message.content === 'Relow info') {
-    message.channel.send('WIP');
-  }
+    message.channel.send("work in progress");
+  }  
   if(message.content === 'relow signup' || message.content === 'Relow signup') {
-    message.author.send('WIP');
+    message.author.send('work in progress');
   }
 if (message.content === 'relow join') {
   message.author.send("_Welcome to the official RELOW discord server! Whether you're here to chat with the community/devs, receive updates or both._\n\n"
 
 + "__QUESTIONS?__\n"
 + "Use the <#484619958395863050> channel.\n\n"
-
+                     
++ "Follow the development also on Twitter and YouTube\n"
++ "https://twitter.com/brokenjoints_\n"
++ "https://www.youtube.com/channel/UCtJGCNnXi0KGdRtvN_QBITQ\nn"
+                     
 + "**Have a nice day and invite to support the game!**\n"
 + "https://discord.gg/bCUvtnz");
 }
