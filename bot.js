cb61488b4fd717c8633385a90f65fa44586722a8
brokenjoints <45832753+brokenjoints@users.noreply.github.com>
2019-04-16 16:30:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,12 +26,12 @@ client.on('message', message => {
   
   if (message.author.bot) return undefined;
   
-  if (message.content.toLowerCase() === 'relow key' && message.channel.id == '528232787514490890') {  
+  if (message.content === 'relow key' && message.channel.id == '528232787514490890') {  
     message.channel.send(message.author.toString() + " is now a **playtester**");
     message.author.send("**Congratulations " + message.author.toString() + "!**\n You are now a **playtester.**\n\n__You will receive the alpha key as soon as possible.__");
   }
   
-  if (message.content.toLowerCase() === 'relow join') {  
+  if (message.content === 'relow join') {  
     message.channel.send("**WELCOME TO THE OFFICIAL RELOW DISCORD SERVER!**\n\n"
 
 + "__ALPHA TESTING__\n"
