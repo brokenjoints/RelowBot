7d48df7e4311ab1485b2fd6591ec562b8bcf6e75
brokenjoints <45832753+brokenjoints@users.noreply.github.com>
2019-04-22 04:09:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ client.on('message', message => {
   
   if (message.author.bot) return undefined;
   
-  if (message.content === 'relow key' && message.channel.id == '528232787514490890') {  
+  if ((message.content === 'relow key' || message.content === 'Relow key') && message.channel.id == '528232787514490890') {  
     message.channel.send(message.author.toString() + " is now a **playtester**");
     message.author.send("**Congratulations " + message.author.toString() + "!**\n You are now a **playtester.**\n\n__You will receive the alpha key as soon as possible.__");
   }
